80ddaffa3fd936af14069d4fb9d6a5599bede671
Starexify <nightbear843@yahoo.com>
2025-01-18 15:01:25 +0100

    21.4.63-beta added EntityType registers

--- src/main/java/net/nova/cosmicore/init/CEntities.java
+++ src/main/java/net/nova/cosmicore/init/CEntities.java
@@ -1,40 +1,26 @@
 package net.nova.cosmicore.init;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.nova.cosmicore.Cosmicore;
 import net.nova.cosmicore.entity.Achondrite;
 import net.nova.cosmicore.entity.Meteorite;
-
-import java.util.function.Supplier;
 
 import static net.nova.cosmicore.Cosmicore.MODID;
 
 public class CEntities {
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, MODID);
+    public static final DeferredRegister.Entities ENTITY_TYPES = DeferredRegister.createEntities(MODID);
 
-    public static final Supplier<EntityType<Achondrite>> ACHONDRITE = register("achondrite",
-            EntityType.Builder.of(Achondrite::new, MobCategory.MISC)
+    public static final DeferredHolder<EntityType<?>, EntityType<Achondrite>> ACHONDRITE = ENTITY_TYPES.registerEntityType("achondrite",
+            Achondrite::new, MobCategory.MISC, meteoriteBuilder -> meteoriteBuilder
                     .sized(1.4F, 1.0F)
                     .clientTrackingRange(10)
-                    .updateInterval(20)
-    );
+                    .updateInterval(20));
 
-    public static final Supplier<EntityType<Meteorite>> METEORITE = register("meteorite",
-            EntityType.Builder.of(Meteorite::new, MobCategory.MISC)
+    public static final DeferredHolder<EntityType<?>, EntityType<Meteorite>> METEORITE = ENTITY_TYPES.registerEntityType("meteorite",
+            Meteorite::new, MobCategory.MISC, meteoriteBuilder -> meteoriteBuilder
                     .sized(1.4F, 1.0F)
                     .clientTrackingRange(10)
-                    .updateInterval(20)
-    );
-
-    // Register
-    public static <T extends Entity> Supplier<EntityType<T>> register(String name, EntityType.Builder<T> builder) {
-        return ENTITY_TYPES.register(ResourceKey.create(Registries.ENTITY_TYPE, Cosmicore.rl(name)).location().getPath(),
-                () -> builder.build(ResourceKey.create(Registries.ENTITY_TYPE, Cosmicore.rl(name)))
-        );
-    }
+                    .updateInterval(20));
 }
